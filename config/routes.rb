0f7ff66f83dfae97ec675454c3aac2adbd4901a4
ksj <ksjee17@gmail.com>
2021-12-01 02:00:55 +0000

    #1 add user&login system 211201

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :movie_evaluations do
     collection do
       get 'search'
@@ -6,5 +7,8 @@ Rails.application.routes.draw do
   end
   resources :tops
   root to: 'tops#index'
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
