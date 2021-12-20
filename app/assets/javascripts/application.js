3e3da49276a248473031a9b8b79acd370a94a0da
ksj <ksjee17@gmail.com>
2021-12-20 15:52:27 +0000

    test2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,18 +27,18 @@
 
 
 
-
-$(window).on('scroll', function() {
-  scrollHeight = $(document).height();
-  scrollPosition = $(window).height() + $(window).scrollTop();
-  if ( (scrollHeight - scrollPosition) / scrollHeight <= 0.05) {
-    $('.jscroll').jscroll({
-      contentSelector: '.scroll-list',
-      nextSelector: 'span.next:last a'
-    });
-  }
+document.addEventListener("turbolinks:load", function () {
+  $(window).on('scroll', function() {
+    scrollHeight = $(document).height();
+    scrollPosition = $(window).height() + $(window).scrollTop();
+    if ( (scrollHeight - scrollPosition) / scrollHeight <= 0.05) {
+      $('.jscroll').jscroll({
+        contentSelector: '.scroll-list',
+        nextSelector: 'span.next:last a'
+      });
+    }
+  });
 });
-
 
 /*!
  * Raty - A Star Rating Plugin
